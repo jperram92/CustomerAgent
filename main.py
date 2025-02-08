eed53f95d9021f11e689f844f3f3bf3dcfd43da4
jperram92 <93854423+jperram92@users.noreply.github.com>
2025-02-08 12:27:14 +1100

    Updated the main file to reference the correct endpoint and relevant data to return from the query

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ load_dotenv() # Load the environment variables
 BASE_API_URL = "https://api.langflow.astra.datastax.com"
 LANGFLOW_ID = "fc5863d6-b718-4ba7-9158-d43ea09066fd"
 FLOW_ID = "e5e837db-ff19-45a1-ae69-4d1df9d262da"
-APPLICATION_TOKEN = "OS.environ.get('APP_TOKEN')"
-ENDPOINT = "" # You can set a specific endpoint name in the flow settings
+APPLICATION_TOKEN = os.environ.get('APP_TOKEN')
+ENDPOINT = "Customer" # You can set a specific endpoint name in the flow settings
 
 
 def run_flow(message: str) -> dict:
@@ -22,3 +22,6 @@ def run_flow(message: str) -> dict:
     headers = {"Authorization": "Bearer " + APPLICATION_TOKEN, "Content-Type": "application/json"}
     response = requests.post(api_url, json=payload, headers=headers)
     return response.json()
+
+result = run_flow('What are the shipping times?')
+print(result)
